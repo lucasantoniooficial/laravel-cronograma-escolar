4e1d16e86444c038d276fa424583be81c44133ae
Lucas Antonio <lucas.antonio@ezoom.com.br>
2021-11-03 22:20:13 -0300

    Template login concluido e dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,24 +11,50 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
+        <link rel="stylesheet" href="{{ mix('css/plugins/fontawesome/all.min.css') }}">
+        <link rel="stylesheet" href="{{mix('css/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+    <body class="hold-transition sidebar-mini layout-fixed">
+        <div class="wrapper">
+
+            <div class="preloader flex-column justify-content align-items-center">
+                <p class="animation__shake">Admin LTE</p>
+            </div>
+
             @include('layouts.navigation')
 
             <!-- Page Heading -->
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
+
 
             <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+            <div class="content-wrapper">
+                <div class="content-header">
+                    <div class="container-fluid">
+                       <div class="row mb-2">
+                           <div class="col-sm-6">
+                               {{ $header }}
+                           </div>
+                           <div class="col-sm-6">
+                               <ol class="breadcrumb float-sm-right">
+                                   {{$breadcrumb ?? null}}
+                               </ol>
+                           </div>
+                       </div>
+                    </div>
+                </div>
+                <section class="content">
+                    {{ $slot }}
+                </section>
+            </div>
+            <footer class="main-footer">
+                <strong>Copyright &copy; {{date('Y')}} <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+                All rights reserved.
+{{--                <div class="float-right d-none d-sm-inline-block">--}}
+{{--                    <b>Version</b> 3.2.0-rc--}}
+{{--                </div>--}}
+            </footer>
         </div>
 
         <!-- Scripts -->
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,12 +11,12 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
+        <link rel="stylesheet" href="{{ mix('css/plugins/fontawesome/all.min.css') }}">
+        <link rel="stylesheet" href="{{mix('css/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     </head>
-    <body>
-        <div class="font-sans text-gray-900 antialiased">
-            {{ $slot }}
-        </div>
+    <body class="hold-transition login-page">
+        {{ $slot }}
 
         <!-- Scripts -->
         <script src="{{ mix('js/app.js') }}"></script>
